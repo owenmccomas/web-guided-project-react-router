a1b7616c3adbed2c5f8e714fdc2ece555286cc29
Owen McComas <owen@mccomas.me>
2023-01-07 17:05:44 -0700

    All instructions completed

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import App from './components/App'
 import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 render(
   // Wrap the <App /> in a provider
-  <App />
+  <Router>
+      <App />
+  </Router>
+
   , document.querySelector('#root')
 )
